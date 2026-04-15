93bd191db995af4cf4ed088d4c5c2026cdd32d6b
James Nuñez <aalejandrobernal@gmail.com>
2026-04-15 14:58:26 -0500

    ubicacion del footer abajo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <script setup>
+import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
 </script>
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-dark  ">
-      <div class="container">
-        <router-link class="navbar-brand fw-bold text-star" to="/">EPICBUILD <br> CONSTRUCTIONS INC</router-link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="menu">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/">Start</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/servicios">Services</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/contacto">Contact</router-link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <router-view />
+  <div class="app-container">
+    <Navbar />
+    <main class="main-content">
+      <router-view />
+    </main>
+    <Footer />
   </div>
-  <Footer />
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark  ">
+        <div class="container">
+            <router-link class="navbar-brand fw-bold text-star" to="/">EPICBUILD <br> CONSTRUCTIONS INC</router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/">Start</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/servicios">Services</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/contacto">Contact</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
